4723f98fcd57f936a234b126927f0573396da0bc
rubanovs <112646924+rubanovs@users.noreply.github.com>
2023-11-05 18:43:28 +0300

    Create Consts, Runner, Vernam, Vigenere

---
+++ src/Runner.py
@@ -0,0 +1,58 @@
+import sys
+import src.Caesar as Caesar
+import src.Vigenere as Vigenere
+import src.Vernam as Vernam
+import src.Consts as Consts
+
+
+class Runner:
+    def __init__(self):
+        self.filename = ""
+        self.key_filename = ""
+        self.number_of_cipher = 0
+        self.mode = 0
+        self.shift_value = 0
+
+    def run_caesar(self):
+        self.shift_value = int(sys.argv[4])
+        cipher = Caesar.Caesar(self.shift_value)
+        if self.mode == Consts.Modes.encryption:
+            Caesar.Caesar.encrypt(cipher, self.filename)
+        elif self.mode == Consts.Modes.decryption:
+            Caesar.Caesar.decrypt(cipher, self.filename)
+        elif self.mode == Consts.Modes.frequency_analysis:
+            Caesar.Caesar.frequency_analysis(cipher, self.filename)
+
+    def run_vigenere(self):
+        self.key_filename = sys.argv[4]
+        cipher = Vigenere.Vigenere()
+        if self.mode == Consts.Modes.encryption:
+            Vigenere.Vigenere.encrypt(cipher, self.filename, self.key_filename)
+        elif self.mode == Consts.Modes.decryption:
+            Vigenere.Vigenere.decrypt(cipher, self.filename, self.key_filename)
+
+    def run_vernam(self):
+        self.key_filename = sys.argv[4]
+        cipher = Vernam.Vernam()
+        if self.mode == Consts.Modes.encryption:
+            Vernam.Vernam.encrypt(cipher, self.filename, self.key_filename)
+        elif self.mode == Consts.Modes.decryption:
+            Vernam.Vernam.decrypt(cipher, self.filename, self.key_filename)
+
+    def run(self):
+        if not len(sys.argv) == Consts.Constants.number_of_args:
+            print("Wrong amount of arguments")
+            sys.exit()
+
+        self.number_of_cipher = int(sys.argv[1])
+        self.mode = int(sys.argv[2])
+        self.filename = sys.argv[3]
+
+        if self.number_of_cipher == Consts.Ciphers.caesar:
+            self.run_caesar()
+
+        elif self.number_of_cipher == Consts.Ciphers.vigenere:
+            self.run_vigenere()
+
+        elif self.number_of_cipher == Consts.Ciphers.vernam:
+            self.run_vernam()
